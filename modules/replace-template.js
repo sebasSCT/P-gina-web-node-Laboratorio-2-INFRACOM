85d506154b74c1f93bc98a95756fba0d5766ea8d
SantiiGC <santiago.garciac1@uqvirtual.edu.co>
2023-04-06 14:54:49 -0500

    Descripcion 3 y detallitos bonitos

--- modules/replace-template.js
+++ modules/replace-template.js
@@ -1,12 +1,13 @@
 module.exports = (temp, product) =>
 {
-    let output = temp.replace(/{%TEMA%}/g, product.tema);
+    let output = temp.replace(/{%TEMA%}/g, product.nombre);
     output = output.replace(/{%IMAGE%}/g, product.image);
     output = output.replace(/{%WEBIMAGE%}/g, product.webimage);
     output = output.replace(/{%WEBIMAGE1%}/g, product.webimage1);
     output = output.replace(/{%WEBVIDEO%}/g, product.webvideo);
     output = output.replace(/{%DESCRIPTION1%}/g, product.description1);
     output = output.replace(/{%DESCRIPTION2%}/g, product.description2);
+    output = output.replace(/{%DESCRIPTION3%}/g, product.description3);
     output = output.replace(/{%ID%}/g, product.id);
 
     //if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
