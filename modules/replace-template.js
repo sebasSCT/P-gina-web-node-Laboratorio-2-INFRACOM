74ea8a9094594b3d9a1e3cc2d6cf64980060f3ea
SantiiGC <santiago.garciac1@uqvirtual.edu.co>
2023-04-05 21:01:09 -0500

    Hola amigos de YT

--- modules/replace-template.js
+++ modules/replace-template.js
@@ -2,12 +2,9 @@ module.exports = (temp, product) =>
 {
     let output = temp.replace(/{%TEMA%}/g, product.tema);
     output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%WEBIMAGE%}/g, product.webimage)
-    output = output.replace(/{%WEBVIDEO%}/g, product.webvideo)
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%WEBIMAGE%}/g, product.webimage);
+    output = output.replace(/{%WEBIMAGE1%}/g, product.webimage1);
+    output = output.replace(/{%WEBVIDEO%}/g, product.webvideo);
     output = output.replace(/{%DESCRIPTION1%}/g, product.description1);
     output = output.replace(/{%DESCRIPTION2%}/g, product.description2);
     output = output.replace(/{%ID%}/g, product.id);
